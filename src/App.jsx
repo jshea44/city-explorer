121129e10ae3a6c8e692ef72266e05ae68dee0a8
joshuashea <joshshea44@gmail.com>
2023-08-21 16:10:46 -0700

    making request to API/updating page with info

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Form from "react-bootstrap/Form";
+import { Button, ListGroupItem } from "react-bootstrap";
+// import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import axios from "axios";
+import "./App.css";
+import ListGroup from "react-bootstrap/ListGroup";
+const API_KEY = import.meta.env.VITE_LOCATIONIQ_API_KEY;
 
-function App() {
-  const [count, setCount] = useState(0)
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      location: null,
+      searchQuery: "",
+    };
+  }
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  handleForm = (e) => {
+    console.log("Form handled");
+    e.preventDefault();
+    axios
+      .get(
+        `https://us1.locationiq.com/v1/search?key=${API_KEY}&q=${this.state.searchQuery}&format=json`
+      )
+      .then((response) => {
+        console.log("SUCCESS: ", response.data);
+        this.setState({ location: response.data[0] });
+      })
+      .catch((error) => {
+        console.log("ERROR: ", error);
+      });
+  };
+
+  handleChange = (e) => {
+    this.setState({ searchQuery: e.target.value }, () =>
+      console.log(this.state.searchQuery)
+    );
+  };
+
+  render() {
+    return (
+      <>
+        <header>
+          <h1>City Explorer!</h1>
+        </header>
+
+        <Form onSubmit={this.handleForm}>
+          <Form.Label>Name Of City</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter city"
+            name="city"
+            onChange={this.handleChange}
+          />
+          <Button type="submit">Explore!</Button>
+        </Form>
+        <ListGroup>
+          <ListGroupItem>
+            City: {this.state.location[0].display_name}
+          </ListGroupItem>
+          <ListGroupItem>Latitude: {this.state.location[0].lat} </ListGroupItem>
+          <ListGroupItem>Longitue: {this.state.location[0].lon} </ListGroupItem>
+        </ListGroup>
+      </>
+    );
+  }
 }
 
-export default App
+export default App;
