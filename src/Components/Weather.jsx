8f173b5c9715a14ce3d9c15bf5ff0504f1185a3e
joshuashea <joshshea44@gmail.com>
2023-08-24 17:10:30 -0700

    craeted weather.jsx && recieved data from server

---
+++ src/Components/Weather.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+class Weather extends React.Component {
+  constructor() {
+    super()
+    
+  }
+
+  render() {
+    return (
+      <>
+        {this.props.forecasts ? (
+          this.props.forecasts.map((forecast, idx) => {
+            return <div key={idx}>
+                  <p>{forecast.date}</p>
+                  <p>{forecast.description}</p>
+            </div>
+            
+          })
+          
+        ) : null}
+      
+      </>
+
+    )
+  }
+
+}
+
+export default Weather;
